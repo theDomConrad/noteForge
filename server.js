19675e310694cb8e919b5e6d07807da2fe449dcc
theDomconrad <them.and.us.2013@gmail.com>
2022-10-26 19:14:32 -0700

    fixed bug, onto next bug lets go

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
+const uniqid = require("uniqid")
 const express = require("express")
-const index = require("./public/assets/js/index")
-const notes = require('./public/notes.html');
 const path = require("path")
 const PORT = 3001;
 const app = express();
-const db = require("./db/db.json")
 
-app.listen(PORT, () =>
-  console.log(`listening to ${PORT}`)
-);
 
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(dirname, '/public/index.html'))
-});
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+  console.info(`${req.method} request received to index.html`);
+  res.json(`${req.method} request received to get reviews`);
+}
+);
 
 app.get('/notes', (req, res) => {
-res.sendFile(path.join(dirname, '/public/notes.html'))
-  });
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    ////////CODE BELOW GOOD, WE LIKEY DIS HELPY
+    console.info(`${req.method} request received to notes.html`);
+
+    res.json(`${req.method} request received to get reviews`);
+}
+);
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__filename, 'db.json'))
+    console.info(`${req.method} request received to get files from db.json`);
+    res.json(`${req.method} request received to get reviews`);
+}
+);
+
+app.post('/api/notes', (req, res) => {
+    res.sendFile(path.join(__filename, 'db.json'))
+    console.info(`${req.method} request received to POST to db.json`);
+}
+);
+
+app.listen(PORT, () =>
+  console.log(`listening to ${PORT}`)
+);

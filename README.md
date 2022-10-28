d22625239307945455ef004b0b26eecc11a80218
theDomconrad <them.and.us.2013@gmail.com>
2022-10-27 18:28:22 -0700

    added notes, updated readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# Note Taker!
+
+## Description
+
+Here we have a wonderful, wonderful note taking app. [Here is a link to the application](https://noteforge.herokuapp.com/notes)
+A user may open the app and take notes! 
+
+## How to use
+
+Press the save button on the top right after entering a title and text, their note will be saved on the left. One may even delete their note by pressing the red delete button! It is a very clean, easy to use app deployed through heroku. 
+
+## Screenshot of app
+
+![screenshot](https://media.discordapp.net/attachments/408481106040717322/1035354334122225724/unknown.png)
+
+## the Code!
+Here we are showcasing the function that helps our index.js delete the desired note. We use a basic for loop and the splice() method to achieve desired results - after we have retrieved the data using fs.readfile. 
+
+
+```
+           let idFinder = req.params.id; //variable set to determine id of clicked note
+    fs.readFile('./db/db.json', 'utf8', (err, data) => { //reading our db.json so we may parse it for desired note
+        if (err) {
+            console.error(err); //if there is no error we do the work
+        } else {
+            const choppedNotes = JSON.parse(data);
+            for (x = 0; x < choppedNotes.length; x++) { //for loop which determines if selected note matches the ID
+                if (idFinder === choppedNotes[x].id) {
+                    choppedNotes.splice(x, 1)
+                }
+            }
+```
+
+## Author Links
+[Linkedin](https://www.linkedin.com/in/dominic-conradson-76638b172/)---
+[GitHub](https://github.com/theDomConrad/)---
+[Portfolio](https://thedomconrad.github.io/Dominic-Conradson-Portfolio/)---
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const uniqid = require("uniqid")
+const uniqid = require("uniqid") //id for notes
 const express = require("express")
 const fs = require("fs")
 const path = require('path')
 let noteDB = require("./db/db.json")
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; //port so heroku works
 const app = express();
 
 app.use(express.json());
@@ -15,18 +15,14 @@ app.get('/notes', (req, res) => {
 }
 );
 
-
-
+//routes to note db
 app.get('/api/notes', (req, res) => {
     res.json(noteDB);
-
-    console.info(`${req.method} am i working??`)
-
-    
+    console.info(`${req.method} routing to note database`)
 }
 );
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => { //function for saving user notes
     console.info(`${req.method} request received to add NOTE!!!`);
     const { title, text } = req.body;
     if (title && text) {
@@ -57,31 +53,29 @@ app.post('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
     console.info(`${req.method} request received to DELETE NOTE!!!`); //making sure we are getting in here
-    
-    let idFinder = req.params.id; //variable set to determine id of clicked note
 
+    let idFinder = req.params.id; //variable set to determine id of clicked note
     fs.readFile('./db/db.json', 'utf8', (err, data) => { //reading our db.json so we may parse it for desired note
         if (err) {
             console.error(err); //if there is no error we do the work
         } else {
             const choppedNotes = JSON.parse(data);
-
-            for (x = 0; x<choppedNotes.length; x++) {
-                if (idFinder === choppedNotes[x].id){
+            for (x = 0; x < choppedNotes.length; x++) { //for loop which determines if selected note matches the ID
+                if (idFinder === choppedNotes[x].id) {
                     choppedNotes.splice(x, 1)
                 }
             }
             noteDB = choppedNotes;
-            fs.writeFile('./db/db.json', JSON.stringify(choppedNotes, null, 3),
+            fs.writeFile('./db/db.json', JSON.stringify(choppedNotes, null, 3), //writing the array back into our database after the deletion has been made
                 (chuckErr) =>
                     chuckErr
                         ? console.error(chuckErr)
-                        : console.info('we so good omg becky')
+                        : console.info('complete')
             );
         }
     }
-    ); res.redirect("back");
-})  
+    ); res.redirect("back"); //refreshing the page
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))

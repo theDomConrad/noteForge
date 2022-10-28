2fd38b918d3eadc6c62c02bc3df5a016cb569ab7
theDomconrad <them.and.us.2013@gmail.com>
2022-10-27 17:19:03 -0700

    fixed typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const fs = require("fs")
 const path = require('path')
 let noteDB = require("./db/db.json")
-const PORT = precess.env.port || 3001;
+const PORT = process.env.port || 3001;
 const app = express();
 
 app.use(express.json());
